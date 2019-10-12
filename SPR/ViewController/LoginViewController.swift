323d9db5cdd60c8dc9477faae7acd974316d467a
ng wing yiu <nwy0206@gmail.com>
2019-10-12 14:41:24 +0800

    Improved version

--- SPR/ViewController/LoginViewController.swift
+++ SPR/ViewController/LoginViewController.swift
@@ -24,31 +24,17 @@ final class LoginViewController: UIViewController {
             return
         }
         
+        doLogin(account: account, password: password)
         
-        let urlString: String = "https://localhost:3000/login"
-        let parameters: [String: String] = ["account": account, "password": password]
+    }
+    
+    
+    private func doLogin(account: String, password: String) {
+        let urlString = "https://localhost:3000/login"
         
-        guard let url = URL(string: urlString) else {
-            popFailAlert()
-            return
-        }
-        
-        var request: URLRequest = URLRequest(url: url)
-        
-        do{
-            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: JSONSerialization.WritingOptions())
-        } catch {
-            popFailAlert()
-        }
-        
-        request.httpMethod = "POST"
-        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-        
-        let task = URLSession.shared.dataTask(with: request) { [weak self] (data, response, error) in
-            
+        request(urlString: urlString, parameters: ["account": account, "password": password]) { [weak self](data, error) in
+            guard let self = self else { return }
             DispatchQueue.main.async {
-                guard let self = self else { return }
-                
                 guard error == nil else {
                     self.popFailAlert(msg: error!.localizedDescription)
                     return
@@ -65,22 +51,21 @@ final class LoginViewController: UIViewController {
                 }
                 
                 if result.result ?? false {
-                    DispatchQueue.main.async {
-                        let vc: HomeViewController = HomeViewController()
-                        self.present(vc, animated: true, completion: nil)
-                    }
+                    self.toHomePage()
                 } else {
                     self.popFailAlert()
                 }
             }
+            
         }
-        task.resume()
-        
     }
     
+    private func toHomePage() {
+        let vc = HomeViewController()
+        self.present(vc, animated: true, completion: nil)
+    }
     
     private func popFailAlert(msg: String = "Please try again") {
-        
         let alertController = UIAlertController(title: "Login Fail", message: msg, preferredStyle: .alert)
         
         let okAction = UIAlertAction(title: "OK", style: .default) { [weak self] _ in
@@ -91,7 +76,46 @@ final class LoginViewController: UIViewController {
         alertController.addAction(okAction)
         
         self.present(alertController, animated: true, completion: nil)
+    }
+    
+    private func request(urlString: String, parameters: [String: Any], completion: @escaping (Data?, Error?) -> Void){
+        guard let url = URL(string: urlString) else {
+            completion(nil, nil)
+            return
+        }
+        var request = URLRequest(url: url)
         
+        do{
+            request.httpBody = try JSONSerialization.data(withJSONObject: parameters, options: JSONSerialization.WritingOptions())
+        }catch let error{
+            completion(nil, error)
+        }
+        
+        request.httpMethod = "POST"
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        fetch(request: request, completion: completion)
+    }
+    
+    private func fetch(request: URLRequest, completion: @escaping (Data?, Error?) -> Void){
+        
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard error == nil else {
+                completion(nil, error!)
+                return
+            }
+            
+            guard let data = data else {
+                completion(nil, nil)
+                return
+            }
+            
+            
+            completion(data, nil)
+            
+            
+        }
+        task.resume()
     }
     
     
